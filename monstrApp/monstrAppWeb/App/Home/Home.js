9051c8ff717c8e69e44770cfbedd7e7afa156b0f
taj121 <johnsot@tcd.ie>
2015-02-03 18:00:19 +0000

    Messing around with getting index's from of value from selection

--- monstrApp/monstrAppWeb/App/Home/Home.js
+++ monstrApp/monstrAppWeb/App/Home/Home.js
@@ -8,16 +8,50 @@
         $(document).ready(function () {
             app.initialize();
 
-            $('#get-data-from-selection').click(getDataFromSelection);
+            $('#get--selected-data').click(getDataFromSelection);
         });
     };
 
+    function getText() {
+        return $('#get').val();
+    }
+
+    /*
+    Function that will find a string in an array (selection of cells passed in) and will print the index's 
+    that it appears at. 
+    Could be altered easily to return array of index's (at the moment in string form) that could be used to reference 
+    another array
+    toFind is the item to be searched for in the array
+    result is the array (2D object returned from getting selection)
+    ~Thea
+    */
+    function findIndex(toFind, result) {
+        var indexToFind = [];
+        for (var i = 0; i < result.value.length; i++) {
+            for (var j = 0; j < result.value[i].length; j++) {
+                if (toFind == result.value[i][j]) {
+                    indexToFind.push('[' + i + ']' + '[' + j + ']');
+                }
+            }
+        }
+        if (indexToFind.length > 0) {
+            app.showNotification('The data item ' + toFind + '  is in the Data at ', indexToFind.toString() + '');
+        }
+        else {
+            app.showNotification('The data item ' + toFind + '  is not in the Data', '');
+        }
+    }
+
+
     // Reads data from current document selection and displays a notification
     function getDataFromSelection() {
-        Office.context.document.getSelectedDataAsync(Office.CoercionType.Text,
+        Office.context.document.getSelectedDataAsync(Office.CoercionType.Matrix,
             function (result) {
                 if (result.status === Office.AsyncResultStatus.Succeeded) {
-                    app.showNotification('The selected text is:', '"' + result.value + '"');
+                    //get string value from user
+                    var toFind = getText();
+                    //check if it is in the selection
+                    findIndex(toFind, result);                    
                 } else {
                     app.showNotification('Error:', result.error.message);
                 }
