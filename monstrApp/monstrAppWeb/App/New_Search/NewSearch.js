e591e6cb2bedc513b84bb8b423c24026d18260f0
dhvogel <vogeld@tcd.ie>
2015-02-17 01:37:09 +0000

    Hey guys. Just got back from my trip and have only been able to work on this tonight. I did a couple things here. First I separated the range selection and data input. Second I added a canvas that tells the user what column they entered (will look better later). Third I added some dynamic buttons so that the user can add information one piece at a time not needing to use commas.

--- monstrApp/monstrAppWeb/App/New_Search/NewSearch.js
+++ monstrApp/monstrAppWeb/App/New_Search/NewSearch.js
@@ -8,33 +8,115 @@
             //will add a new column to search in, initially will have nothing to search for ~Thea
             //$('#get-new-column').click(addNewColumn);
             //adds new values to the most resent column added to current search ~Thea
-            //$('#add-search-param').click(addNewParam);
+            //$('#ch-param').click(addNewParam);
             checkLocal();
-            $('#add-search').click(function () {
-                addNewSearch();
+            $('#select_column').click(function () {
+                selectColumn();
             });
+            $('#check_column').click(function () {
+                checkColumn();
+            })
+            $('#add_to_search').click(function () {
+                addToSearch($('#get_param').val());
+            })
+            $('#complete_entry').click(function () {
+                completeEntry();
+            })
         });
     };
 
     //stores the current search array of form column-dataToSearchFor column being index 0 of internal array ~Thea
-    var currentSearch = [];
+    var MasterSearch = [];
+    var CurrentSearch = [];
+    var column = "";
+    var col_saved = false;
+    var term_count = 0;
 
-    function addNewSearch() {
-        var newColIndex = convertLettersToNumbers($('#get-col').val());
-        var param = $('#get-param').val().replace(/\s+/g, ''); //remove all whitespaces
-        var params = param.split(",");
-        var searchCol = [newColIndex, params]; // [X,[1,2,3]]
-        currentSearch.push(searchCol);
-        localStorage["currentSearch"] = JSON.stringify(currentSearch);
+    //Function that selects the column and fills the canvas with the column letter. Dan
+    function selectColumn() {
+        col_saved = true;
+        column = $('#get_col').val();
+        var c=document.getElementById("myCanvas");
+        var ctx = c.getContext("2d");
+        ctx.clearRect(0,0,200,100);
+        ctx.font = "20px Georgia";
+        ctx.fillText("Column " + column.toUpperCase() + " selected", 10, 50);
+        CurrentSearch = [];
+        //Then want to highlight column is possible -- will most likely require input
+        //from initial data selection.
+        //Office.context.document.setSelectedDataAsync(Office.CoercionType.Matrix)  
     }
 
+    //Function that checks to make sure the user entered in an acceptable column. Dan
+    function checkColumn() {
+        if (column = "" || col_saved == false) {
+
+            return false;
+        }
+        var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        for (var i = 0; i < column.length; i++) {
+            if (column[i] in alphabet) {
+                continue;
+            }
+            else
+                return false;
+        }
+        col_saved = false;
+        return true;
+
+    }
+    
+    //Adds terms one at a time to search, dynamically generating buttons to show the user what is in
+    //the current search. Clicking on the buttons makes them disappear and then they are removed from the
+    //current search as well. Dan
+    function addToSearch(term) {
+        term_count++;
+        if (term != -1) {
+            //terms pushed to current search as they are entered 
+            CurrentSearch.push(term);
+            app.showNotification(CurrentSearch);
+        }
+
+        var $button = $('<button/>', {
+            type: 'button',
+            'class': 'dynBtn',
+            id: term_count,
+            text: term,
+            value: term,
+            click: function () {
+                app.showNotification("Removed term '" + $(this).attr("value") + "' from search");
+                
+                var index = CurrentSearch.indexOf($(this).attr("value"));
+                if (index > -1) {
+                    //terms spliced from current search as they are deleted
+                    CurrentSearch.splice(index, 1);
+                }
+                $(this).remove();
+                
+            }
+        });
+        $button.appendTo('#button_div');
+        $('#get_param').val("");
+    }
+
+    function completeEntry() {
+        app.showNotification(column);
+        var newColIndex = convertLettersToNumbers(column);
+        MasterSearch = [newColIndex, CurrentSearch];
+        app.showNotification(MasterSearch);
+        localStorage["CurrentSearch"] = JSON.stringify(MasterSearch);
+
+    }
+
+       
+
     function checkLocal() {
-        currentSearch = localStorage.getItem("currentSearch");
-        if (currentSearch || currentSearch === "") {
-            currentSearch = JSON.parse(localStorage["currentSearch"]);
+        CurrentSearch = localStorage.getItem("CurrentSearch");
+        if (CurrentSearch || CurrentSearch === "") {
+            CurrentSearch = JSON.parse(localStorage["CurrentSearch"]);
         }
         else {
-            currentSearch = [];
+            CurrentSearch = [];
         }
     }
 
